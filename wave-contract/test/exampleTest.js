fcd4bdc7bcbd691d56fbaca1f83b7bf810d122aa
J0xhua <joshua_data@protonmail.com>
2022-04-28 16:58:45 +0100

    Pushing the wave portal to a repo, to share with buildspace trainers

---
+++ wave-contract/test/exampleTest.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+
+describe("myExampleTest", () => {
+    it("test1", () => {
+        const a = 1;
+        const b = 1;
+        expect(a ).to.equal(b)
+    })
+})
+
